f3d3a1f01c5761ee85f8bafb6583848d2c50810d
Daniëlle <danielle.de.kloet@hva.nl>
2018-11-23 16:18:52 +0100

    23-11

--- script.js
+++ script.js
@@ -4,16 +4,21 @@
 
 /*eslint 'no-console': 0*/
 
-//document.querySelector('button').addEventListener('click', function() {
+//var button = document.querySelectorAll('button');
+//
+//for (var i = 0; i < button.length; i++) {
+//    button[i].addEventListener('click', function() {
 //  document.querySelector('.content').style.height= 'auto';
 //  this.style.display= 'none';
-//});
+//    });
+//}
 
-var button = document.querySelectorAll('button');
+$(function(){
 
-for (var i = 0; i < button.length; i++) {
-    button[i].addEventListener('click', function() {
-  document.querySelector('.content').style.height= 'auto';
-  this.style.display= 'none';
+    $("button").on("click", function(){
+
+        $(this).prev(".content").toggleClass("test");
+
+
     });
-}
+});
